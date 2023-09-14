64d389428bdb304da5ff4707813d4ce4f5ccb779
Yellowinastrip <giliera1997@gmail.com>
2023-09-14 11:29:18 +0200

    bug fix

--- patterns.js
+++ patterns.js
@@ -29,9 +29,8 @@ export const spider = {
     {stick: [['b14', true]], candy: [['r43', true], ['g32', true], ['b24', false]]},
     {stick: [['b14', true], ['r43', true]], candy: [['g32', true], ['r23', false]]},
     {stick: [['b14', true]], candy: [['r43', false], ['g32', true], ['r23', false], ['b34', false]]},
-
   ],
-  inputs: ['g12', 'r13', 'b14', 'g22', 'r23', 'b34', 'g32', 'r33', 'b34', 'g42', 'r43', 'b44'],
+  inputs: ['g12', 'r13', 'b14', 'g22', 'r23', 'b24', 'g32', 'r33', 'b34', 'g42', 'r43', 'b44'],
 };
 
 export const longHigway = {
@@ -60,10 +59,12 @@ export const distributedHigway = {
     {stick: [['g12', true]], candy: [['r23', true], ['b34', true], ['g42', false]]},
     {stick: [['g12', true]], candy: [['b23', true], ['g34', false], ['r42', true]]},
     {stick: [['g12', true]], candy: [['b23', true], ['r34', true], ['g42', false]]},
+    
     {stick: [['r13', true]], candy: [['g34', true], ['r42', false], ['b23', true]]},
     {stick: [['r13', true]], candy: [['g34', true], ['b42', true], ['r23', false]]},
     {stick: [['r13', true]], candy: [['b34', true], ['r42', false], ['g23', true]]},
     {stick: [['r13', true]], candy: [['b34', true], ['g42', true], ['r23', false]]},
+    
     {stick: [['b14', true]], candy: [['g42', true], ['b23', false], ['r34', true]]},
     {stick: [['b14', true]], candy: [['g42', true], ['r23', true], ['b34', false]]},
     {stick: [['b14', true]], candy: [['r42', true], ['b23', false], ['g34', true]]},
